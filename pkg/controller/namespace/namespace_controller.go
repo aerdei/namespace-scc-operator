02e45ce51526ee381210d1a9d3b266bd617879f6
aerdei <aerdei@redhat.com>
2019-09-19 14:49:25 +0200

    Secondary resource are the SCCs, owner is the ns

--- pkg/controller/namespace/namespace_controller.go
+++ pkg/controller/namespace/namespace_controller.go
@@ -50,9 +50,8 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
-	// TODO(user): Modify this to be the types you create that are owned by the primary resource
-	// Watch for changes to secondary resource Pods and requeue the owner Namespace
-	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
+	// Watch for changes to secondary resource SCCs and requeue the owner MapRScc
+	err = c.Watch(&source.Kind{Type: &securityv1.SecurityContextConstraints{}}, &handler.EnqueueRequestForOwner{
 		IsController: true,
 		OwnerType:    &corev1.Namespace{},
 	})
