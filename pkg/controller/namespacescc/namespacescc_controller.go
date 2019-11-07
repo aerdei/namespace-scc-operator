cab692179679f6c345dbbab6b1c23f01fe045eb4
Attila Erdei <aerdei@redhat.com>
2019-11-07 11:54:55 +0100

    Add controller logic

--- pkg/controller/namespacescc/namespacescc_controller.go
+++ pkg/controller/namespacescc/namespacescc_controller.go
@@ -4,11 +4,13 @@ import (
 	"context"
 
 	namespacesccv1alpha1 "github.com/aerdei/namespace-scc-operator/pkg/apis/namespacescc/v1alpha1"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	securityv1 "github.com/openshift/api/security/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -51,11 +53,16 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
-	// TODO(user): Modify this to be the types you create that are owned by the primary resource
-	// Watch for changes to secondary resource Pods and requeue the owner NamespaceSCC
-	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
+	// Watch for changes to primary resource Namespace
+	err = c.Watch(&source.Kind{Type: &corev1.Namespace{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to secondary resource SCCs and requeue the owner MapRScc
+	err = c.Watch(&source.Kind{Type: &securityv1.SecurityContextConstraints{}}, &handler.EnqueueRequestForOwner{
 		IsController: true,
-		OwnerType:    &namespacesccv1alpha1.NamespaceSCC{},
+		OwnerType:    &corev1.Namespace{},
 	})
 	if err != nil {
 		return err
@@ -86,10 +93,9 @@ func (r *ReconcileNamespaceSCC) Reconcile(request reconcile.Request) (reconcile.
 	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
 	reqLogger.Info("Reconciling NamespaceSCC")
 
-	// Fetch the NamespaceSCC instance
-	instance := &namespacesccv1alpha1.NamespaceSCC{}
-	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
-	if err != nil {
+	// Fetch the NamespaceSCC CR list
+	sccList := &namespacesccv1alpha1.NamespaceSCCList{}
+	if err := r.client.List(context.TODO(), sccList); err != nil {
 		if errors.IsNotFound(err) {
 			// Request object not found, could have been deleted after reconcile request.
 			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
@@ -100,54 +106,128 @@ func (r *ReconcileNamespaceSCC) Reconcile(request reconcile.Request) (reconcile.
 		return reconcile.Result{}, err
 	}
 
-	// Define a new Pod object
-	pod := newPodForCR(instance)
-
-	// Set NamespaceSCC instance as the owner and controller
-	if err := controllerutil.SetControllerReference(instance, pod, r.scheme); err != nil {
-		return reconcile.Result{}, err
-	}
-
-	// Check if this Pod already exists
-	found := &corev1.Pod{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, found)
-	if err != nil && errors.IsNotFound(err) {
-		reqLogger.Info("Creating a new Pod", "Pod.Namespace", pod.Namespace, "Pod.Name", pod.Name)
-		err = r.client.Create(context.TODO(), pod)
-		if err != nil {
-			return reconcile.Result{}, err
+	// Fetch the Namespace list
+	nsList := &corev1.NamespaceList{}
+	if err := r.client.List(context.TODO(), nsList); err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return reconcile.Result{}, nil
 		}
-
-		// Pod created successfully - don't requeue
-		return reconcile.Result{}, nil
-	} else if err != nil {
+		// Error reading the object - requeue the request.
 		return reconcile.Result{}, err
 	}
-
+	// For every SCC, check if every namespace has a corresponding scc. If not, create one. If present but differs, update.
+	for _, sccElement := range sccList.Items {
+		for _, nsElement := range nsList.Items {
+			whiteListedNamespace := false
+			for _, whiteListed := range sccElement.Spec.WhiteList {
+				if nsElement.Name == whiteListed {
+					whiteListedNamespace = true
+				}
+			}
+			if !whiteListedNamespace {
+				// Reconcile SCC
+				reqLogger.Info("Defining a new SCC object")
+				scc := r.newSCCForNS(&sccElement, &nsElement)
+				reqLogger.Info("Checking if this SCC already exists")
+				// Get the SCC instance
+				sccfound := &securityv1.SecurityContextConstraints{}
+				if err := r.client.Get(context.TODO(), client.ObjectKey{Name: "mapr-" + nsElement.Name}, sccfound); err != nil && errors.IsNotFound(err) {
+					// Define a new SCC object
+					reqLogger.Info("SCC not found - reating a new SCC")
+					err = r.client.Create(context.TODO(), scc)
+					if err != nil {
+						return reconcile.Result{}, err
+					}
+					// SCC created successfully - don't requeue
+					reqLogger.Info("SCC created successfully - not requeuing")
+					//return reconcile.Result{}, nil
+				} else if err != nil {
+					reqLogger.Info("SCC get error - requeueing")
+					return reconcile.Result{}, err
+				} else if !equalSCCs(sccfound, scc) {
+					reqLogger.Info("SCC mismatch - updating")
+					sccfound = scc
+					err = r.client.Update(context.TODO(), sccfound)
+					if err != nil {
+						reqLogger.Error(err, "Failed to update SCC")
+						return reconcile.Result{}, err
+					}
+					//return reconcile.Result{}, nil
+				}
+			}
+		}
+	}
 	// Pod already exists - don't requeue
-	reqLogger.Info("Skip reconcile: Pod already exists", "Pod.Namespace", found.Namespace, "Pod.Name", found.Name)
 	return reconcile.Result{}, nil
 }
 
-// newPodForCR returns a busybox pod with the same name/namespace as the cr
-func newPodForCR(cr *namespacesccv1alpha1.NamespaceSCC) *corev1.Pod {
+// newSCCForNS returns an SCC with the name mapr-{namespace}
+func (r *ReconcileNamespaceSCC) newSCCForNS(cr *namespacesccv1alpha1.NamespaceSCC, ns *corev1.Namespace) *securityv1.SecurityContextConstraints {
 	labels := map[string]string{
-		"app": cr.Name,
+		"namespace": ns.Name,
 	}
-	return &corev1.Pod{
+	scc := &securityv1.SecurityContextConstraints{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      cr.Name + "-pod",
-			Namespace: cr.Namespace,
-			Labels:    labels,
+			Name:   "mapr-" + ns.Name,
+			Labels: labels,
 		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:    "busybox",
-					Image:   "busybox",
-					Command: []string{"sleep", "3600"},
+		AllowPrivilegedContainer: false,
+		AllowHostNetwork:         false,
+		AllowHostPorts:           false,
+		AllowHostPID:             false,
+		AllowHostIPC:             false,
+		Priority:                 &cr.Spec.SccPriority,
+		FSGroup: securityv1.FSGroupStrategyOptions{
+			Type: securityv1.FSGroupStrategyMustRunAs,
+			Ranges: []securityv1.IDRange{
+				securityv1.IDRange{
+					Min: cr.Spec.UUID,
+					Max: cr.Spec.UUID,
 				},
 			},
 		},
+		ReadOnlyRootFilesystem: false,
+		RequiredDropCapabilities: []corev1.Capability{
+			"KILL",
+			"MKNOD",
+			"SETUID",
+			"SETGID",
+		},
+		RunAsUser: securityv1.RunAsUserStrategyOptions{
+			Type: securityv1.RunAsUserStrategyMustRunAs,
+			UID:  &cr.Spec.UUID,
+		},
+		SELinuxContext: securityv1.SELinuxContextStrategyOptions{
+			Type: securityv1.SELinuxStrategyMustRunAs,
+		},
+		SupplementalGroups: securityv1.SupplementalGroupsStrategyOptions{
+			Type: securityv1.SupplementalGroupsStrategyRunAsAny,
+			Ranges: []securityv1.IDRange{
+				securityv1.IDRange{
+					Min: cr.Spec.UUID,
+					Max: cr.Spec.UUID,
+				},
+			},
+		},
+		Volumes: []securityv1.FSType{
+			securityv1.FSTypeConfigMap,
+			securityv1.FSTypeDownwardAPI,
+			securityv1.FSTypeEmptyDir,
+			securityv1.FSTypePersistentVolumeClaim,
+			securityv1.FSProjected,
+			securityv1.FSTypeSecret,
+		},
+		Users:  []string{"system:serviceaccount:" + ns.Name + ":default"},
+		Groups: []string{"mapr-sas"},
 	}
+	controllerutil.SetControllerReference(ns, scc, r.scheme)
+	return scc
+}
+
+// newSCCForNS only returns true if all fields except TypeMeta and ObjectMeta in sccfound and scc are equal
+func equalSCCs(sccfound *securityv1.SecurityContextConstraints, scc *securityv1.SecurityContextConstraints) bool {
+	return (cmp.Equal(sccfound, scc, cmpopts.IgnoreFields(securityv1.SecurityContextConstraints{}, "TypeMeta", "ObjectMeta")))
 }
